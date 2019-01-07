61962733067db117be9817023261665d94939f6a
Eric Salmon <ersalmon1@verizon.net>
2019-01-07 01:19:31 -0500

    Mailgun Setup

--- config/env/production.js
+++ config/env/production.js
@@ -11,7 +11,7 @@ module.exports = {
   },
   port: process.env.PORT || 8443,
   // Binding to 127.0.0.1 is safer in production.
-  host: process.env.HOST || '127.0.0.1',
+  host: process.env.HOST || '0.0.0.0',
   db: {
     //uri: process.env.MONGOHQ_URL || process.env.MONGODB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/mean',
     uri: 'mongodb://159.203.125.111:27017/mean',
